2e67e7b38ba32948bf754a200a06b4c7f16e0be8
xzhang <xzhang@nvtests-MacBook-Pro.local>
2022-05-10 11:47:54 -0700

    NVSHAS-6371: Vulnerability - support-log gzip file accessible via GET without authentication

---
+++ common/src/main/scala/com/neu/cache/SupportLogAuthCacheManager.scala
@@ -0,0 +1,31 @@
+package com.neu.cache
+import net.sf.ehcache.CacheManager
+
+object SupportLogAuthCacheManager {
+  implicit val cacheKeyGenerator: ToStringCacheKeyGenerator.type = ToStringCacheKeyGenerator
+  implicit val cacheManager: CacheManager                        = CacheManager.getInstance()
+
+  val cacheName = "supportLogAuthCache"
+
+  val cache: Cache[String, String] =
+    Ehcache[String, String](cacheName)
+
+  /**
+   * Save supportLogAuth for support log
+   */
+   def saveSupportLogAuth(token: String, filename: String): Unit = {
+     cache.put(token, filename)
+   }
+
+  /**
+   * Get supportLogAuth for support log
+   * @param token
+   * @return [[String]]
+   */
+   def getSupportLogAuth(token: String): Option[String] = cache.get(token)
+
+   /**
+    * Remove supportLogAuth for support log
+    */
+  def removeSupportLogAuth(token: String): Unit = cache.remove(token)
+}

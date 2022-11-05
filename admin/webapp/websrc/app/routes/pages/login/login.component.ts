a54bca7ac35e2cc21e19d7e75f48ac18c04c0da0
Steven Zhang <stzhang@Stevens-MacBook-Pro.local>
2022-11-05 13:29:02 -0700

    NVSHAS-6841: Changing the UI landing page (with login) to original style

--- admin/webapp/websrc/app/routes/pages/login/login.component.ts
+++ admin/webapp/websrc/app/routes/pages/login/login.component.ts
@@ -260,6 +260,7 @@ export class LoginComponent implements OnInit {
 
         this.validEula = this.isEulaAccepted;
         // GlobalVariable.isOpenShift = false;
+        this.isEulaAccepted = false;
       },
       error => {
         this.cookieService.delete('temp');

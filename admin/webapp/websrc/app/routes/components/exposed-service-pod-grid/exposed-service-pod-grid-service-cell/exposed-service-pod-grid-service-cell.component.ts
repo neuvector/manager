d506777130075ea2f6d8f3c01a563ed35e717124
Steven Zhang <stzhang@Stevens-MacBook-Pro.local>
2022-11-06 09:07:21 -0800

    NVSHAS-6850: warning sign and color code is missing in Ingress and Egress Exposure section of dashboard page

--- admin/webapp/websrc/app/routes/components/exposed-service-pod-grid/exposed-service-pod-grid-service-cell/exposed-service-pod-grid-service-cell.component.ts
+++ admin/webapp/websrc/app/routes/components/exposed-service-pod-grid/exposed-service-pod-grid-service-cell/exposed-service-pod-grid-service-cell.component.ts
@@ -12,6 +12,7 @@ export class ExposedServicePodGridServiceCellComponent implements ICellRendererA
   params!: ICellRendererParams;
   name!: string;
   isParent!: boolean;
+  rowStyle: any;
   get isChildVisible() {
     return this.params.data.visible;
   }
@@ -22,6 +23,7 @@ export class ExposedServicePodGridServiceCellComponent implements ICellRendererA
     this.params = params;
     this.name = params.data.isParent ? params.data.service : params.data.display_name;
     this.isParent = params.data.isParent;
+    this.rowStyle = this.getServicePodStyle(this.params);
   }
 
   refresh(params: ICellRendererParams): boolean {
@@ -35,5 +37,77 @@ export class ExposedServicePodGridServiceCellComponent implements ICellRendererA
       if (child_node) child_node.data.visible = !child_node.data.visible;
     });
     this.params.api.onFilterChanged();
+  }
+
+  private getServicePodStyle = params => {
+    const colorArray = [
+      "text-danger",
+      "text-warning",
+      "text-caution",
+      "text-monitor",
+      "text-protect",
+    ];
+    const levelMap = {
+      protect: 4,
+      monitor: 3,
+      discover: 2,
+      violate: 1,
+      warning: 1,
+      deny: 0,
+      critical: 0,
+    };
+    const actionTypeIconMap = {
+      discover: "fa icon-size-2 fa-exclamation-triangle",
+      violate: "fa icon-size-2 fa-ban",
+      protect: "fa icon-size-2 fa-shield",
+      monitor: "fa icon-size-2 fa-bell",
+      deny: "fa icon-size-2 fa-minus-circle",
+      threat: "fa icon-size-2 fa-bug",
+    };
+
+    let level: number[] = [];
+    if (this.isParent) {
+      if (this.params.data.severity) {
+        level.push(levelMap[this.params.data.severity.toLowerCase()]);
+      } else if (
+        this.params.data.policy_action &&
+        (this.params.data.policy_action.toLowerCase() === "deny" ||
+          this.params.data.policy_action.toLowerCase() === "violate")
+      ) {
+        level.push(levelMap[this.params.data.policy_action.toLowerCase()]);
+      } else {
+        if (!this.params.data.policy_mode) this.params.data.policy_mode = "discover";
+        level.push(levelMap[this.params.data.policy_mode.toLowerCase()]);
+      }
+      let serviceColor = colorArray[Math.min(...level)];
+      return {
+        color: serviceColor,
+        actionIcon: ''
+      };
+    } else {
+      let actionType = "";
+      let level = 0;
+      if (this.params.data.severity) {
+        level = levelMap[this.params.data.severity.toLowerCase()];
+        actionType = actionTypeIconMap["threat"];
+      } else if (
+        this.params.data.policy_action &&
+        (this.params.data.policy_action.toLowerCase() === "deny" ||
+          this.params.data.policy_action.toLowerCase() === "violate")
+      ) {
+        level = levelMap[this.params.data.policy_action.toLowerCase()];
+        actionType =
+          actionTypeIconMap[this.params.data.policy_action.toLowerCase()];
+      } else {
+        if (!this.params.data.policy_mode) this.params.data.policy_mode = "discover";
+        level = levelMap[this.params.data.policy_mode.toLowerCase()];
+        actionType =
+          actionTypeIconMap[this.params.data.policy_mode.toLowerCase()];
+      }
+      return {
+        color: colorArray[level],
+        actionIcon: actionType
+      };
+    }
   }
 }

aa09ae3dd7a674d1393a0030d6ad54e0c30f72ff
lsongsuse <long.song@suse.com>
2023-02-23 15:24:02 -0800

    NVSHAS-7581 When add/edit group, the filter should be editable

--- admin/webapp/websrc/app/routes/components/groups/partial/add-edit-group-modal/add-edit-group-modal.component.ts
+++ admin/webapp/websrc/app/routes/components/groups/partial/add-edit-group-modal/add-edit-group-modal.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef,  MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatDialog,
+} from '@angular/material/dialog';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { FormControl, FormGroup, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ValidatorFn,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 import { GlobalConstant } from '@common/constants/global.constant';
 import { MatChipInputEvent } from '@angular/material/chips';
 import { GroupsService } from '@services/groups.service';
 import { TranslateService } from '@ngx-translate/core';
 import { NotificationService } from '@services/notification.service';
+import { InputDialogComponent } from '@components/ui/input-dialog/input-dialog.component';
 
 @Component({
   selector: 'app-add-edit-group-modal',
   templateUrl: './add-edit-group-modal.component.html',
-  styleUrls: ['./add-edit-group-modal.component.scss']
+  styleUrls: ['./add-edit-group-modal.component.scss'],
 })
 export class AddEditGroupModalComponent implements OnInit {
-
   modalOp: any;
   separatorKeysCodes: number[] = [ENTER, COMMA];
   addEditGroupForm: FormGroup;
@@ -26,34 +37,35 @@ export class AddEditGroupModalComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public data: any,
     private groupsService: GroupsService,
     private translate: TranslateService,
-    private notificationService: NotificationService
-  ) { }
+    private notificationService: NotificationService,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.modalOp = GlobalConstant.MODAL_OP;
     if (this.data.opType === GlobalConstant.MODAL_OP.ADD) {
       this.addEditGroupForm = new FormGroup({
         name: new FormControl(
+          this.data.cfgType === GlobalConstant.CFG_TYPE.FED ? 'fed.' : '',
           this.data.cfgType === GlobalConstant.CFG_TYPE.FED
-            ? 'fed.'
-            : '',
-          this.data.cfgType === GlobalConstant.CFG_TYPE.FED
-            ? [
-                Validators.required,
-                this.fedNameValidator()
-              ]
+            ? [Validators.required, this.fedNameValidator()]
             : Validators.required
         ),
         comment: new FormControl(''),
-        criteriaCtrl: new FormControl()
+        criteriaCtrl: new FormControl(),
       });
     } else {
       this.addEditGroupForm = new FormGroup({
-        name: new FormControl(this.data.selectedGroup.name, Validators.required),
+        name: new FormControl(
+          this.data.selectedGroup.name,
+          Validators.required
+        ),
         comment: new FormControl(this.data.selectedGroup.comment),
-        criteriaCtrl: new FormControl()
+        criteriaCtrl: new FormControl(),
       });
-      this.criteria = JSON.parse(JSON.stringify(this.data.selectedGroup.criteria));
+      this.criteria = JSON.parse(
+        JSON.stringify(this.data.selectedGroup.criteria)
+      );
       if (this.data.opType === GlobalConstant.MODAL_OP.VIEW) {
         this.addEditGroupForm.controls.criteriaCtrl.disable();
       } else {
@@ -65,7 +77,7 @@ export class AddEditGroupModalComponent implements OnInit {
   fedNameValidator(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
       const isValid = control.value.startsWith('fed.');
-      return isValid ? null : {fedName: {value: control.value}};
+      return isValid ? null : { fedName: { value: control.value } };
     };
   }
 
@@ -76,7 +88,7 @@ export class AddEditGroupModalComponent implements OnInit {
   addCriterion = (event: MatChipInputEvent) => {
     const value = (event.value || '').trim();
     if (value) {
-      this.criteria.push({name: value});
+      this.criteria.push({ name: value });
     }
 
     event.chipInput!.clear();
@@ -89,19 +101,37 @@ export class AddEditGroupModalComponent implements OnInit {
     }
   };
 
+  editCriterion = (criterion: any) => {
+    const index = this.criteria.indexOf(criterion);
+    const dialogRef = this.dialog.open(InputDialogComponent, {
+      data: {
+        input: criterion.name,
+        title: this.translate.instant('group.EDIT_CRITERIA'),
+      },
+    });
+    dialogRef.afterClosed().subscribe(input => {
+      const value = (input || '').trim();
+      if (value) {
+        this.criteria[index].name = value;
+      }
+    });
+  };
+
   updateGroup = () => {
     console.log(this.addEditGroupForm.value);
     let payload = {
       ...this.addEditGroupForm.value,
       criteria: this.criteria,
-      cfg_type: this.data.cfgType
-    }
-    this.groupsService.insertUpdateGroupData(payload, this.data.opType)
+      cfg_type: this.data.cfgType,
+    };
+    this.groupsService
+      .insertUpdateGroupData(payload, this.data.opType)
       .subscribe(
         response => {
-          let msgTitle = this.data.opType === GlobalConstant.MODAL_OP.ADD ?
-            this.translate.instant('group.addGroup.OK_MSG') :
-            this.translate.instant('group.editGroup.OK_MSG');
+          let msgTitle =
+            this.data.opType === GlobalConstant.MODAL_OP.ADD
+              ? this.translate.instant('group.addGroup.OK_MSG')
+              : this.translate.instant('group.editGroup.OK_MSG');
           this.notificationService.open(msgTitle);
           setTimeout(() => {
             this.data.refresh();
@@ -109,12 +139,12 @@ export class AddEditGroupModalComponent implements OnInit {
           this.dialogRef.close(true);
         },
         error => {
-          let msgTitle = this.data.opType === GlobalConstant.MODAL_OP.ADD ?
-            this.translate.instant('group.addGroup.ERR_MSG') :
-            this.translate.instant('group.editGroup.ERR_MSG');
+          let msgTitle =
+            this.data.opType === GlobalConstant.MODAL_OP.ADD
+              ? this.translate.instant('group.addGroup.ERR_MSG')
+              : this.translate.instant('group.editGroup.ERR_MSG');
           this.notificationService.openError(error, msgTitle);
         }
-      )
+      );
   };
-
 }

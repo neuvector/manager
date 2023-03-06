90b79588d5419f1c0922d08ccc41c7ca3f681b63
Rush Kapoor <rush.kapoor@berkeley.edu>
2023-03-06 14:53:42 -0500

    NVSHAS-7335: Allow NeuVector to set several ADFS certificates in parallel in x.509 certificate field.
    Adds UI support for SAML config field `x509_cert_extras`

--- admin/src/main/scala/com/neu/model/SamlServer.scala
+++ admin/src/main/scala/com/neu/model/SamlServer.scala
@@ -7,6 +7,7 @@ case class SamlServer(
   sso_url: String,
   issuer: String,
   x509_cert: Option[String],
+  x509_cert_extras: Option[Array[String]],
   group_claim: Option[String] = Some(""),
   enable: Option[Boolean],
   default_role: Option[String],

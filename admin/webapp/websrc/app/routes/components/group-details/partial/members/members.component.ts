95a7f79e560f12f84c4c9bd626d747fc33db7296
Bin Xu <bin.xu@suse.com>
2023-04-26 00:16:30 -0700

    :bug: Fix Jira case: https://jira.suse.com/browse/NVSHAS-7833, fix issue on combo while applying blacklist.

--- admin/webapp/websrc/app/routes/components/group-details/partial/members/members.component.ts
+++ admin/webapp/websrc/app/routes/components/group-details/partial/members/members.component.ts
@@ -1,28 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ContainersService } from '@common/services/containers.service';
 
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
-  styleUrls: ['./members.component.scss']
+  styleUrls: ['./members.component.scss'],
 })
 export class MembersComponent implements OnInit {
-
-  @Input() source: string;
+  @Input() source: string = '';
   @Input() groupName: string = '';
-  @Input() resizableHeight: number;
+  @Input() resizableHeight: number = 0;
   @Input() members: any;
-  @Input() kind: string;
-  @Input() useQuickFilterService: boolean;
+  @Input() kind: string = '';
+  @Input() useQuickFilterService: boolean = false;
   memberGridRowData: any;
 
-  constructor(
-    private containersService: ContainersService
-  ) { }
+  constructor(private containersService: ContainersService) {}
 
   ngOnInit(): void {
-    this.memberGridRowData =  this.kind === 'node' ? this.members : this.containersService.formatScannedWorkloads(this.members);
-    console.log(this.memberGridRowData)
+    this.memberGridRowData =
+      this.kind === 'node'
+        ? this.members
+        : this.containersService.formatScannedWorkloads(this.members);
   }
-
 }

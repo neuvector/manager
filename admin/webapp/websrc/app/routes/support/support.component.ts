e6d9d6e1220b3096cc9fd7c594cb71d2f589b711
lsongsuse <long.song@suse.com>
2023-11-22 07:57:34 -0800

    NVSHAS-8360 Add "Get Support" to NeuVector Menu

---
+++ admin/webapp/websrc/app/routes/support/support.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-support',
+  templateUrl: './support.component.html',
+  styleUrls: ['./support.component.scss'],
+})
+export class SupportComponent {}

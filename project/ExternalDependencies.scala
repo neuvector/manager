23be562049b9128c4b0995d90cbf3d21651f58dd
Nick Chung <Nick.Chung@suse.com>
2024-09-11 17:39:11 +0800

    chore: update pekko version 1.0.2 to 1.0.3

--- project/ExternalDependencies.scala
+++ project/ExternalDependencies.scala
@@ -21,7 +21,8 @@ object ExternalDependencies {
          .exclude("com.typesafe", "config")
   }
 
-  val sprayV         = "1.3.3"
+  val sprayV = "1.3.3"
+  val pekkoV = "1.0.3"
 
   val typesafeConfig = "com.typesafe"               % "config"         % "1.3.0"
   val slf4jLog4j     = "org.slf4j"                  % "slf4j-log4j12"  % "1.7.21"
@@ -32,18 +33,15 @@ object ExternalDependencies {
   val ehCache        = "net.sf.ehcache"    % "ehcache"     % "2.10.6"
   val guava          = "com.google.guava"  % "guava"       % "16.0.1"
 
-  private val ioSpray: String = "io.spray"
-  val sprayJson               = ioSpray %% "spray-json" % "1.3.6" exclude ("org.scala-lang", "scala-library")
-
-  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.16" % "test" scalaTestExclude
-  // val spec2     = "org.specs2" %% "specs2" % "3.7" % "test"
-
-
-  val pekkoV = "1.0.2"
-
   val pekkoActor   = "org.apache.pekko" %% "pekko-actor"   % pekkoV pekkoExclude
   val pekkoHttp   = "org.apache.pekko" %% "pekko-http"   % "1.0.1"
   val pekkoJson = "org.apache.pekko" %% "pekko-http-spray-json" % "1.0.1"
   val pekkoSlf4j   = "org.apache.pekko" %% "pekko-slf4j"   % pekkoV
   val pekkoStream  = "org.apache.pekko" %% "pekko-stream"  % pekkoV
+
+  private val ioSpray: String = "io.spray"
+  val sprayJson               = ioSpray %% "spray-json" % "1.3.6" exclude ("org.scala-lang", "scala-library")
+
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.16" % "test" scalaTestExclude
+  val spec2     = "org.specs2" %% "specs2" % "3.7" % "test"
 }

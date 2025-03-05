dad4df1a8075e1f775811463f2566bcd0de7926a
Nick Chung <nick.chung@suse.com>
2025-03-05 11:27:57 +0800

    bugfix: missing Scala version adjustment of Dockerfile and make_jar.sh scripts

--- make_jar.sh
+++ make_jar.sh
@@ -43,5 +43,5 @@ fi
 # npm run unittest
 popd
 env JAVA_OPTS="-Xms2g -Xmx3g" sbt admin/assembly
-zip -d admin/target/scala-3.3.4/admin-assembly-1.0.jar rest-management-private-classpath\*
+zip -d admin/target/scala-3.3.5/admin-assembly-1.0.jar rest-management-private-classpath\*
 rm -rf admin/webapp/root/.sass-cache

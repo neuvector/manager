xingzhang-suse/manager-1

NVSHAS-10065: Add a configuration setting on UI for allowing ns user to export

admin/src/main/scala/com/neu/model/SystemConfig.scala
@@ -120,6 +120,7 @@ case class SystemConfigSyslogCfgV2(
 
 case class SystemConfigAuthCfgV2(
   auth_order: Option[Array[String]] = None,
+  allow_ns_user_export_net_policy: Option[Boolean] = None,
   auth_by_platform: Option[Boolean] = None,
   rancher_ep: Option[String] = None
 )

admin/webapp/websrc/app/routes/settings/configuration/config-form/config-form-config/constants/config-constants.ts
@@ -437,6 +437,18 @@ export const DisableNetworkPolicyToggleBoolField = {
   },
 };
 
+export const NsUserExportNetworkRuleToggleField = {
+  key: 'auth.allow_ns_user_export_net_policy',
+  type: FormlyComponents.TOGGLE,
+  templateOptions: {
+    ariaLabelledBy: 'setting.NS_USER_EXPORT_NET_POLICY',
+  },
+  expressionProperties: {
+    'templateOptions.disabled':
+      '!formState.permissions.isNsUserExportNetworkRuleAuthorized',
+  },
+};
+
 export const DisableNetworkPolicyToggleField = {
   key: 'net_svc.disable_net_policy',
 };

admin/webapp/websrc/app/routes/settings/configuration/config-form/config-form.component.ts
@@ -123,6 +123,7 @@ export class ConfigFormComponent implements OnInit {
       isConfigAuthorized: isSettingAuth,
       isIBMSAAuthorized: isSettingAuth,
       isTlsAuthorized: isSettingAuth,
+      isNsUserExportNetworkRuleAuthorized: isSettingAuth,
     };
     this.serverErrorMessage = '';
     this.cd.detectChanges();
@@ -224,6 +225,7 @@ export class ConfigFormComponent implements OnInit {
         auth_cfg: {
           // NOTE: auth_cfg = {} - formly is missing auth_cfg fields
           auth_order: base_config.auth.auth_order,
+          allow_ns_user_export_net_policy: base_config.auth.allow_ns_user_export_net_policy,
           auth_by_platform: base_config.auth.auth_by_platform,
           rancher_ep: base_config.auth.rancher_ep,
         },

neuvector/manager

admin/src/main/scala/com/neu/model/SystemConfig.scala
@@ -151,7 +151,8 @@ case class SystemConfigMiscCfgV2(
   monitor_service_mesh: Option[Boolean] = None,
   xff_enabled: Option[Boolean] = None,
   no_telemetry_report: Option[Boolean] = None,
-  csp_type: Option[String] = None
+  csp_type: Option[String] = None,
+  allow_ns_user_export_net_policy: Option[Boolean] = None
 )
 
 case class SystemConfigTlsCfg(

admin/webapp/websrc/app/routes/settings/configuration/config-form/config-form-config/constants/config-constants.ts
@@ -437,6 +437,18 @@ export const DisableNetworkPolicyToggleBoolField = {
   },
 };
 
+export const NsUserExportNetworkRuleToggleField = {
+  key: 'misc.allow_ns_user_export_net_policy',
+  type: FormlyComponents.TOGGLE,
+  templateOptions: {
+    ariaLabelledBy: 'setting.NS_USER_EXPORT_NET_POLICY',
+  },
+  expressionProperties: {
+    'templateOptions.disabled':
+      '!formState.permissions.isNsUserExportNetworkRuleAuthorized',
+  },
+};
+
 export const DisableNetworkPolicyToggleField = {
   key: 'net_svc.disable_net_policy',
 };

admin/webapp/websrc/app/routes/settings/configuration/config-form/config-form.component.ts
@@ -123,6 +123,7 @@ export class ConfigFormComponent implements OnInit {
       isConfigAuthorized: isSettingAuth,
       isIBMSAAuthorized: isSettingAuth,
       isTlsAuthorized: isSettingAuth,
+      isNsUserExportNetworkRuleAuthorized: isSettingAuth,
     };
     this.serverErrorMessage = '';
     this.cd.detectChanges();
@@ -271,6 +272,7 @@ export class ConfigFormComponent implements OnInit {
           monitor_service_mesh: base_config.misc.monitor_service_mesh,
           xff_enabled: base_config.misc.xff_enabled,
           no_telemetry_report: base_config.misc.no_telemetry_report,
+          allow_ns_user_export_net_policy: base_config.misc.allow_ns_user_export_net_policy,
         },
         tls_cfg: {
           enable_tls_verification: base_config.tls['enable_tls_verification'],
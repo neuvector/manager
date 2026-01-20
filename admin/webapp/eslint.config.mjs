c0e2e2a9c5799d95ca545c981024783bfdbbbbaa
Rush Kapoor <rush.kapoor@suse.com>
2026-01-20 12:45:59 -0800

    [Ag20] Update .nvmrc to Node.js > v20 #1103
    Update .nvmrc
    Migrate to ESLint flat config
    Upgrade linting/formatting deps

---
+++ admin/webapp/eslint.config.mjs
@@ -0,0 +1,73 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import angularDataTestId from 'eslint-plugin-angular-data-test-id';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  globalIgnores([
+    '**/dist/**',
+    '**/node_modules/**',
+    '**/root/**',
+    '**/assets/**',
+    '.angular/**',
+    '**/*.woff',
+    '**/*.woff2',
+  ]),
+
+  {
+    files: ['**/*.ts'],
+    extends: [
+      ...compat.extends(
+        'plugin:@angular-eslint/recommended',
+        'plugin:@angular-eslint/template/process-inline-templates'
+      ),
+      eslintPluginPrettierRecommended,
+    ],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parserOptions: {
+        project: ['tsconfig.json'],
+        createDefaultProgram: true,
+      },
+    },
+    rules: {},
+  },
+
+  {
+    files: ['**/*.html'],
+    extends: [...compat.extends('plugin:@angular-eslint/template/recommended')],
+    plugins: {
+      'angular-data-test-id': angularDataTestId,
+    },
+    rules: {
+      'angular-data-test-id/data-test-id': 'off',
+    },
+  },
+
+  {
+    files: ['**/*.html'],
+    ignores: ['**/*inline-template-*.component.html'],
+    extends: [eslintPluginPrettierRecommended],
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          parser: 'angular',
+        },
+      ],
+    },
+  },
+]);

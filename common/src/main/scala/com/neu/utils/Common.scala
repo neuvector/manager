3e705c8a849d0a162807aaed446533d54f5e8851
Steven Zhang <steven.zhang@suse.com>
2026-03-11 18:13:39 -0700

    NVSHAS-10256: Add user validation on the APIs which does not communicate with controller (Fixed PR review issues)

--- common/src/main/scala/com/neu/utils/Common.scala
+++ common/src/main/scala/com/neu/utils/Common.scala
@@ -6,8 +6,8 @@ object Common {
   def shortKey(jwt: String): String = {
     val digest = MessageDigest.getInstance("SHA-256")
     digest
-      .digest(jwt.getBytes("UTF-8"))
-      .take(20)
+      .digest(jwt.getBytes(java.nio.charset.StandardCharsets.UTF_8))
+      .take(10)
       .map("%02x".format(_))
       .mkString
   }

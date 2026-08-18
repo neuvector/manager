0f08b6a7ffc478aa22070794a4712f2dcf462203
Steven Zhang <steven.zhang@suse.com>
2026-08-17 19:31:40 -0700

    #1276 Feature Request: NeuVector pod logs in JSON format

--- admin/src/main/scala/com/neu/core/Core.scala
+++ admin/src/main/scala/com/neu/core/Core.scala
@@ -6,6 +6,7 @@ import com.neu.web.StaticResources
 import com.typesafe.config.{ Config, ConfigValueFactory }
 import com.typesafe.config.ConfigFactory.*
 import com.typesafe.scalalogging.LazyLogging
+import org.apache.log4j.PropertyConfigurator
 import org.apache.pekko.actor.ActorSystem
 import org.apache.pekko.http.scaladsl.ConnectionContext
 import org.apache.pekko.http.scaladsl.Http
@@ -14,6 +15,7 @@ import org.apache.pekko.http.scaladsl.server.Route
 import org.apache.pekko.http.scaladsl.settings.ServerSettings
 import org.apache.pekko.stream.Materializer
 
+import java.util.Properties
 import scala.concurrent.ExecutionContextExecutor
 import scala.concurrent.Future
 
@@ -31,11 +33,62 @@ trait BootedCore
   given materializer: Materializer                 = Materializer(system)
   given executionContext: ExecutionContextExecutor = system.dispatcher
 
+  private def configureLog4j(): Unit = {
+    val logFormat   = sys.env.getOrElse("NV_LOG_FORMAT", "string").trim.toLowerCase
+    val layoutClass = logFormat match {
+      case "json"             => "net.logstash.log4j.JSONEventLayoutV1"
+      case "string" | "plain" => "org.apache.log4j.PatternLayout"
+      case _                  =>
+        logger.warn(s"Unsupported NV_LOG_FORMAT=$logFormat; defaulting to string format")
+        "org.apache.log4j.PatternLayout"
+    }
+    val props       = new Properties()
+    val stream      = Option(getClass.getClassLoader.getResourceAsStream("log4j.properties"))
+    stream.foreach { in =>
+      props.load(in)
+      in.close()
+    }
+
+    props.setProperty("log4j.rootLogger", "INFO, stdout")
+    props.setProperty("log4j.appender.stdout.layout", layoutClass)
+    if (layoutClass != "org.apache.log4j.PatternLayout") {
+      props.remove("log4j.appender.stdout.layout.ConversionPattern")
+    }
+
+    props.setProperty("log4j.appender.adminAppender.layout", layoutClass)
+    if (layoutClass != "org.apache.log4j.PatternLayout") {
+      props.remove("log4j.appender.adminAppender.layout.ConversionPattern")
+    }
+
+    props.setProperty("log4j.appender.kafkaAppender.layout", layoutClass)
+    if (layoutClass != "org.apache.log4j.PatternLayout") {
+      props.remove("log4j.appender.kafkaAppender.layout.ConversionPattern")
+    }
+
+    props.setProperty("log4j.appender.pekkoAppender.layout", layoutClass)
+    if (layoutClass != "org.apache.log4j.PatternLayout") {
+      props.remove("log4j.appender.pekkoAppender.layout.ConversionPattern")
+    }
+
+    props.setProperty("log4j.appender.sourceAppender.layout", layoutClass)
+    if (layoutClass != "org.apache.log4j.PatternLayout") {
+      props.remove("log4j.appender.sourceAppender.layout.ConversionPattern")
+    }
+
+    props.setProperty("log4j.appender.sinkAppender.layout", layoutClass)
+    if (layoutClass != "org.apache.log4j.PatternLayout") {
+      props.remove("log4j.appender.sinkAppender.layout.ConversionPattern")
+    }
+
+    PropertyConfigurator.configure(props)
+  }
+
   private val rootService: Route = routes ~ staticResources
 
   IpGeoManager
   CisNISTManager
   System.setProperty("net.sf.ehcache.enableShutdownHook", "true")
+  configureLog4j()
 
   private val useSSL: String                    = sys.env.getOrElse("MANAGER_SSL", "on")
   private val httpMaxHeaderLength: String       = sys.env.getOrElse("HTTP_MAX_HEADER_LENGTH", "32k")
